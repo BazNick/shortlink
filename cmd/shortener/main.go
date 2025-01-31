3167e6b88fca62c864e46e693e9b648571416df0
Nick <nikita_tyupin@mail.ru>
2025-01-31 14:41:37 +0300

    Short Link Iter_1 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/BazNick/shortlink/internal/app/api"
+)
+
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/`, api.AddLink)
+	mux.HandleFunc(`/{id}`, api.GetLink)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
