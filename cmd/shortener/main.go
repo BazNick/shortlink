42c900224cdcb29bed27f0ced10166a651de06e7
Nick <nikita_tyupin@mail.ru>
2025-02-01 22:48:02 +0300

    Iter2 add tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"net/http"
 
-	"github.com/BazNick/shortlink/internal/app/api"
+	"github.com/BazNick/shortlink/internal/app/handlers"
 )
 
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(`/`, api.AddLink)
-	mux.HandleFunc(`/{id}`, api.GetLink)
+	mux.HandleFunc(`/`, handlers.AddLink)
+	mux.HandleFunc(`/{id}`, handlers.GetLink)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
