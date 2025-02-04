42d2cd9d69b9d924da5204f6e52aee65d6a5c2ea
Nick <nikita_tyupin@mail.ru>
2025-02-04 18:22:30 +0300

    Iter5 done

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,19 +1,34 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"log"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type Config struct {
-	Address string
-	BaseURL string
+	Address string `env:"ADDRESS"`
+	BaseURL string `env:"BASE_URL"`
 }
 
 func GetCLParams() Config {
 	var config Config
 
+	err := env.Parse(&config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.BaseURL != "" && config.Address != "" {
+		return config
+	}
+
 	flag.StringVar(&config.Address, "a", "localhost:8080", "Адрес запуска HTTP-сервера")
 	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "base URL")
 
 	flag.Parse()
-	
+
 	return config
 }
