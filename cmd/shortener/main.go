c6dba885999b66d31316f61d6578b99c17ac895c
Nick <nikita_tyupin@mail.ru>
2025-02-04 18:10:29 +0300

    Iter4 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"github.com/BazNick/shortlink/cmd/config"
 	"github.com/BazNick/shortlink/internal/app/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
+	conf := config.GetCLParams()
+
 	router := gin.Default()
 
-    router.GET("/:id", handlers.GetLink)
-    router.POST("/", handlers.AddLink)
+	router.GET("/:id", handlers.GetLink)
+	router.POST("/", handlers.AddLink)
 
-    router.Run(":8080")
+	router.Run(conf.Address)
 }
