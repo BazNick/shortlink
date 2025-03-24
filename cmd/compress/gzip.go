146d2df3975a63ebdccb2a51b9eb18ec9e2a36a5
Nick <nikita_tyupin@mail.ru>
2025-03-24 09:05:35 -0500

    iter8 done

---
+++ cmd/compress/gzip.go
@@ -0,0 +1,40 @@
+package compress
+
+import (
+	"compress/gzip"
+	"io"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type gzipWriter struct {
+	gin.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func GzipHandle() gin.HandlerFunc {
+	return gin.HandlerFunc(func(c *gin.Context) {
+		if !strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
+			c.Next()
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(c.Writer, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(c.Writer, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		c.Writer.Header().Set("Content-Encoding", "gzip")
+
+		c.Writer = gzipWriter{ResponseWriter: c.Writer, Writer: gz}
+
+		c.Next()
+	})
+}
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/BazNick/shortlink/cmd/config"
 	"github.com/BazNick/shortlink/cmd/logger"
+	"github.com/BazNick/shortlink/cmd/compress"
 	"github.com/BazNick/shortlink/internal/app/entities"
 	"github.com/BazNick/shortlink/internal/app/handlers"
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,7 @@ func main() {
 	conf := config.GetCLParams()
 
 	router := gin.Default()
-	router.Use(logger.WithLogging())
+	router.Use(logger.WithLogging(), compress.GzipHandle())
 
 	hashDict := entities.NewHashDict()
 	urlHandler := handlers.NewURLHandler(hashDict)
