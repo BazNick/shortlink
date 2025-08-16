b0521ef230c055a2ea81ce6e92af5cd66b29f3e2
Nick <nikita_tyupin@mail.ru>
2025-08-16 16:55:57 +0300

    iter16 done

--- internal/app/entities/filestore.go
+++ internal/app/entities/filestore.go
@@ -3,9 +3,8 @@ package entities
 import (
 	"bufio"
 	"encoding/json"
-	"io"
-	"log"
 	"os"
+	"sync"
 )
 
 type FileLinks struct {
@@ -16,106 +15,88 @@ type FileLinks struct {
 type FileStore struct {
 	Path        string
 	FileStorage *os.File
+	mu          sync.RWMutex
+	cache       map[string]string
 }
 
 func NewFileStore(path string) *FileStore {
-	return &FileStore{
-		Path: path,
+	fs := &FileStore{
+		Path:  path,
+		cache: make(map[string]string),
+	}
+	
+	fs.loadCache()
+	return fs
+}
+
+func (f *FileStore) loadCache() {
+	file, err := os.OpenFile(f.Path, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var res FileLinks
+		if err := json.Unmarshal([]byte(scanner.Text()), &res); err == nil {
+			f.cache[res.ShortURL] = res.OriginalURL
+		}
 	}
 }
 
 func (f *FileStore) AddHash(hash, link, userID string) (string, error) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+
+	if _, exists := f.cache[hash]; exists {
+		return hash, nil
+	}
+
+	f.cache[hash] = link
+
 	file, err := os.OpenFile(f.Path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return "", err
 	}
-
-	bufWriter := bufio.NewWriter(file)
+	defer file.Close()
 
 	data, err := json.Marshal(FileLinks{
 		ShortURL:    hash,
 		OriginalURL: link,
 	})
 	if err != nil {
-		file.Close()
 		return "", err
 	}
 
-	if _, err = bufWriter.Write(data); err != nil {
-		file.Close()
-		return "", err
-	}
-
-	if _, err = bufWriter.WriteRune('\n'); err != nil {
-		file.Close()
-		return "", err
-	}
-
-	if err := bufWriter.Flush(); err != nil {
-		file.Close()
-		return "", err
-	}
-
-	if err := file.Sync(); err != nil {
-		file.Close()
-		return "", err
-	}
+	writer := bufio.NewWriter(file)
+	writer.Write(data)
+	writer.WriteRune('\n')
+	writer.Flush()
 
 	return "", nil
 }
 
 func (f *FileStore) GetHash(hash string) string {
-	reader, errOpenFile := os.OpenFile(f.Path, os.O_RDONLY|os.O_CREATE, 0666)
-	if errOpenFile != nil {
-		log.Fatalf("Ошибка при открытии файла %s: %v", f.Path, errOpenFile)
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	
+	if link, exists := f.cache[hash]; exists {
+		return link
 	}
-	defer reader.Close()
-
-	var (
-		r   = bufio.NewReader(reader)
-		dec = json.NewDecoder(r)
-	)
-
-	for {
-		var res FileLinks
-		err := dec.Decode(&res)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Ошибка при открытии файла %s: %v", f.Path, err)
-		}
-		if hash == res.ShortURL {
-			return res.OriginalURL
-		}
-	}
+	
 	return ""
 }
 
 func (f *FileStore) CheckValExists(link string) bool {
-	reader, errOpenFile := os.OpenFile(f.Path, os.O_RDONLY|os.O_CREATE, 0666)
-	if errOpenFile != nil {
-		log.Fatalf("Ошибка при открытии файла %s: %v", f.Path, errOpenFile)
-	}
-	defer reader.Close()
-
-	var (
-		r   = bufio.NewReader(reader)
-		dec = json.NewDecoder(r)
-	)
-
-	for {
-		var res FileLinks
-		err := dec.Decode(&res)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Ошибка при открытии файла %s: %v", f.Path, err)
-		}
-		if link == res.OriginalURL {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	
+	for _, cachedLink := range f.cache {
+		if cachedLink == link {
 			return true
 		}
 	}
+	
 	return false
 }
