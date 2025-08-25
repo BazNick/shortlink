c9a13c6c7ca17986f4c1c2e18560e936d26ef5af
Nick <nikita_tyupin@mail.ru>
2025-08-25 22:09:21 +0300

    iter20 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"runtime"
 
 	"github.com/BazNick/shortlink/cmd/config"
 	"github.com/BazNick/shortlink/cmd/middleware/auth"
@@ -15,7 +15,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
+
 	var (
 		conf    config.Config
 		router  = gin.Default()
@@ -34,8 +42,6 @@ func main() {
 	case conf.DB != "":
 		db := entities.NewDB(conf.DB)
 		storage = db
-
-		entities.StartDeleteWorkers(db.Database, runtime.NumCPU())
 
 		defer db.Database.Close()
 	case conf.FilePath != "":
@@ -70,4 +76,25 @@ func main() {
 	router.DELETE("/api/user/urls", urlHandler.DeleteUserLinks)
 
 	router.Run(conf.Address)
+}
+
+func printBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", version)
+	fmt.Printf("Build date: %s\n", date)
+	fmt.Printf("Build commit: %s\n", commit)
 }
